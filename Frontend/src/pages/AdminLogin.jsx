ced0dcbab70fa670db9139b66ae0495133333586
Abdul Kabeer <abdulkabeerkhoso082@gmail.com>
2026-07-18 21:00:37 +0500

    add console log to admin login

--- Frontend/src/pages/AdminLogin.jsx
+++ Frontend/src/pages/AdminLogin.jsx
@@ -16,7 +16,7 @@ const AdminLogin = () => {
 
     setLoading(true);
     setError("");
-
+    console.log("handle Submit is working");
     try {
       const res = await axios.post(
         `${import.meta.env.VITE_API_URL}/api/admin/login`,
