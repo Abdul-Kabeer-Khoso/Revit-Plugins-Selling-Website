c9f103e6ae9dbd9f48ce00556cd932cfbdd25af9
Abdul Kabeer <abdulkabeerkhoso082@gmail.com>
2026-07-23 19:37:58 +0500

    decomment stripe code

--- Backend/app.js
+++ Backend/app.js
@@ -10,8 +10,8 @@ import downloadRoutes from "./routes/downloadRoutes.js";
 import familiesRoutes from "./routes/familiesRoutes.js";
 import adminRoutes from "./routes/adminRoutes.js";
 import cloudinaryRoutes from "./routes/cloudinaryRoutes.js";
-// import stripeRoutes from "./routes/stripeRoutes.js";
-// import licenseRoutes from "./routes/licenseRoutes.js";
+import stripeRoutes from "./routes/stripeRoutes.js";
+import licenseRoutes from "./routes/licenseRoutes.js";
 
 const app = express();
 
@@ -20,7 +20,7 @@ connectDB();
 app.use(cors());
 
 // Stripe webhook MUST come before express.json()
-// app.post("/api/webhook", express.raw({ type: "application/json" }));
+app.post("/api/webhook", express.raw({ type: "application/json" }));
 
 // Parse JSON for every other route
 app.use(express.json());
@@ -31,8 +31,8 @@ app.use("/api", homeRoutes);
 app.use("/api", downloadRoutes);
 app.use("/api", familiesRoutes);
 app.use("/api", cloudinaryRoutes);
-// app.use("/api", stripeRoutes);
-// app.use("/api", licenseRoutes);
+app.use("/api", stripeRoutes);
+app.use("/api", licenseRoutes);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
