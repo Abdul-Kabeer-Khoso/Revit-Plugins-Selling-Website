519f3a9da4ea9bc4b5d70b70a3409d1ad20005f3
Abdul Kabeer <abdulkabeerkhoso082@gmail.com>
2026-07-23 16:48:21 +0500

    Change download plugin prefix

--- Frontend/src/components/RevitPurchase.jsx
+++ Frontend/src/components/RevitPurchase.jsx
@@ -14,7 +14,7 @@ const RevitPurchase = ({ description, price, id }) => {
 
   return (
     <div className="bg-gray-300 text-sm md:text-lg rounded-lg mt-3 mx-5 py-3 px-5 flex justify-between items-center font-bold border-amber-950">
-      <p>FOR {description}</p>
+      <p>FOR REVIT {description}</p>
       <p>User/Year</p>
       <p>${price}</p>
       <button
